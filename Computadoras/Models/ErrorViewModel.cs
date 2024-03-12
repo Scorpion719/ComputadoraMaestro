5f3777d76a07bb66882b9af1b7390724dd94cb43
Rolando <ae24001@esfe.agape.edu.sv>
2024-03-11 18:03:16 -0600

    Agregar archivos de proyecto.

---
+++ Computadoras/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Computadoras.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
